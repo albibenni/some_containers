8b5801601e7220f9be11630d8de1803973efa972
Alberto Benatti <albi.benni8@gmail.com>
2025-07-06 22:30:52 +0200

    test

--- some_script/test_print_all_args.sh
+++ some_script/test_print_all_args.sh
@@ -3,11 +3,13 @@
 #!/bin/bash
 
 echo '--- Con "$@" ---'
+echo $@
 for arg in "$@"; do
   echo "Argomento: [$arg]"
 done
 
 echo '--- Con "$*" ---'
+echo $*
 for arg in "$*"; do
   echo "Argomento: [$arg]"
 done
